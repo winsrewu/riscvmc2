8ea52c041fc4724ab50a381722e2c763c9c877f5
WisW <78122384+winsrewu@users.noreply.github.com>
2026-09-05 18:25:10 +0800

    chore: remove app/ and use submodule pyriscv instead

--- app/c-test/build.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-riscv32-unknown-elf-gcc -g -static -march=rv32im -mabi=ilp32 main.c -o main.out
-
-riscv32-unknown-elf-gcc -g -static -mrelax -specs=nosys.specs -march=rv32im -mabi=ilp32 -T ../c-common/link.ld -o app.elf ../c-common/vectors.S ../c-common/syscalls.c main.c -lc -lgcc
-riscv32-unknown-elf-objcopy -O verilog app.elf app.mem
-riscv32-unknown-elf-objdump -S -d app.elf > app.lst
